fe2340b0b04a980cebe18db3256fb3d42b031b64
Gonzalo Moreno <gonzalo.moreno@polaris.es>
2022-07-21 10:20:27 +0200

    First upload

---
+++ HtmlToPdf2AZ/Models/MarginOptions.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HtmlToPdf2AZ.Models
+{
+    /// <summary>
+    /// MarginOptions
+    /// </summary>
+    public class MarginOptions : PuppeteerSharp.Media.MarginOptions
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        public MarginOptions()
+        {
+        }
+        /// <summary>
+        /// MarginOptions
+        /// </summary>
+        /// <param name="margin"></param>
+        public MarginOptions(string margin)
+        {
+            Top = margin;
+            Left = margin;
+            Bottom = margin;
+            Right = margin;
+        }
+        /// <summary>
+        /// MarginOptions
+        /// </summary>
+        /// <param name="top"></param>
+        /// <param name="left"></param>
+        /// <param name="bottom"></param>
+        /// <param name="right"></param>
+        public MarginOptions(string top, string left, string bottom, string right)
+        {
+            Top = top;
+            Left = left;
+            Bottom = bottom;
+            Right = right;
+        }
+    }
+}
